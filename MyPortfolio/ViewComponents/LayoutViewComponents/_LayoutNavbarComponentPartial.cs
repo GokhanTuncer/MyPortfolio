41b6002233ac85b0d432f634859c9964ebb30023
GokhanTuncer <gokhan_alphan@hotmail.com>
2025-01-30 13:15:59 +0300

    Navbar bildirim ve sidebar düzenlenmesi

--- MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents.LayoutViewComponents
 {
 	public class _LayoutNavbarComponentPartial : ViewComponent
 	{
+		MyPortfolioContext context = new MyPortfolioContext();
 		public IViewComponentResult Invoke()
 		{
-			return View();
+			ViewBag.ToDoListCount = context.ToDoLists.Where(x => x.Status == false).Count(); //Yapılmamış Bildirim sayısı
+			var value = context.ToDoLists.Where(x => x.Status == false).ToList(); 
+			return View(value);
 		}
 	}
 }
